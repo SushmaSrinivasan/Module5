2c5ddc5577bcb009b1fee70e5c9b4e96431753b7
sushmasrinivasan04@gmail.com <sushmasrinivasan04@gmail.com>
2023-09-14 09:34:59 +0200

    Add project files.

---
+++ Module5/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Module5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int amount = 1234;
+            int months = 12;
+            int yearlyWage=calculateYearlyWage(amount, months);
+            Console.WriteLine($"yearlyWage:{yearlyWage}");  
+            Console.ReadLine();
+            static int calculateYearlyWage(int monthlyWage,int numberOfMonthsWorked)
+            {
+                //Console.WriteLine($"Yearly wage:{monthlyWage * numberOfMonthsWorked}");
+                //return monthlyWage + numberOfMonthsWorked;
+                if(numberOfMonthsWorked == 12) 
+                {
+                    return monthlyWage * (numberOfMonthsWorked + 1);
+                }
+                return monthlyWage * numberOfMonthsWorked;
+            }
+        }
+    }
+}
